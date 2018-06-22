904b0ecd6357b627695f2cf7d16624583056246a
DaiQuest <daiquest.jumoun@gmail.com>
2018-06-22 21:29:29 +0200

    fix login

--- resources/views/elements/navbar.blade.php
+++ resources/views/elements/navbar.blade.php
@@ -20,10 +20,66 @@
                         <li><a href="{{ route('Rcon') }}">Rcon</a></li>
                     @endif
                 @endauth
-            @guest
+                @guest
                 <!-- Large modal -->
-                    <li> <a type="button" href="{{ login }}" class=" nav-link " data-toggle="modal" data-target=".bs-example-modal-lg"> login</a>
+                    <li> <a type="button" class=" nav-link " data-toggle="modal" data-target=".bs-example-modal-lg"> login</a>
+                        <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+                            <div class="modal-dialog modal-lg" role="document">
+                                <div class="modal-content">
+                                    <div class="container-fluid" >
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <form method="POST" action="{{ route('login') }}">
+                                                    @csrf
+                                                    <div class="form-group row">
+                                                        <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                                                        <div class="col-md-6">
+                                                            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                                            @if ($errors->has('email'))
+                                                                <span class="invalid-feedback">
+                                                            <strong>{{ $errors->first('email') }}</strong>
+                                                            </span>
+                                                            @endif
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                                                        <div class="col-md-6">
+                                                            <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                                            @if ($errors->has('password'))
+                                                                <span class="invalid-feedback">
+                                                            <strong>{{ $errors->first('password') }}</strong>
+                                                            </span>
+                                                            @endif
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <div class="col-md-6 offset-md-4">
+                                                            <div class="checkbox">
+                                                                <label>
+                                                                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
+                                                                </label>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row mb-0">
+                                                        <div class="col-md-8 offset-md-4">
+                                                            <button type="submit" class="btn btn-primary">
+                                                                {{ __('Login') }}
+                                                            </button>
 
+                                                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                                                {{ __('Forgot Your Password?') }}
+                                                            </a>
+                                                        </div>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </li>
 
                     <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
